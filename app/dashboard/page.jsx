11fc81477556f44c16c26a48d409b8482e1f1592
jaykant <jaykantmishra88@gmail.com>
2025-06-12 14:58:18 +0530

    solving deploy error

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import WelcomeBanner from './_components/WelcomeBanner';
-import CourseList from './_components/courseList';
+import CourseList from './_components/CourseList'; 
 
 const Dashboard = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen px-4">
-     
       <CourseList />
     </div>
   );
